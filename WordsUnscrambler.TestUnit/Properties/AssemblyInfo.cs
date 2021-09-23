cdf752172008dd293a90b85d0ab2b621170a33a1
Konrad Kowalski <k.kowalski@medicalgorithmics.com>
2021-09-23 16:53:12 +0200

    first commit

---
+++ WordsUnscrambler.TestUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WordsUnscrambler.TestUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Medicalgorithmics")]
+[assembly: AssemblyProduct("WordsUnscrambler.TestUnit")]
+[assembly: AssemblyCopyright("Copyright © Medicalgorithmics 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73fe5120-9a94-43ca-a72c-c29583ed159d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WordsUnscrambler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            int[] myArray = new int[10];
+        }
+    }
+}
