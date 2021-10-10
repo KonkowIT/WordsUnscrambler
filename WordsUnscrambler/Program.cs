037afac2a6617482b80439c3fb4066bbeb0706f4
Konrad Kowalski <konradkowalski95@gmail.com>
2021-10-10 20:03:18 +0100

    update until while loop

--- WordsUnscrambler/Program.cs
+++ WordsUnscrambler/Program.cs
@@ -10,8 +10,32 @@ namespace WordsUnscrambler
     {
         static void Main(string[] args)
         {
+            //-- LISTS
             List<int> myList = new List<int>();
-            int[] myArray = new int[10];
+            //  adding items to list
+            myList.Add(6);
+            //  removing item from list
+            myList.Remove(1); // removes first item where value is 1 
+
+            //-- ARRAYS
+            int[] myArray = new int[3]; // array of 10 items
+            //  adding items to array
+            myArray[0] = 1;
+            myArray[1] = 4;
+            myArray[2] = 3;
+
+            // OR
+            int[] myArray2 = { 1, 4, 3 };
+
+            //  sorting array
+            Array.Sort(myArray);
+
+            //-- FOR LOOP
+            for (int i = 0; i < myArray.Count(); i++)
+            {
+                Console.WriteLine("Step: {0}, value: {1}", i, myArray[i]);
+            }
+
         }
     }
 }
