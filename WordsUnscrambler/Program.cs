b47f93ebb9dafdfeef9e1aaebda021fa53dfd239
Konrad Kowalski <k.kowalski@medicalgorithmics.com>
2021-11-24 16:35:04 +0100

    update

--- WordsUnscrambler/Program.cs
+++ WordsUnscrambler/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WordsUnscrambler
 {
@@ -10,58 +11,15 @@ namespace WordsUnscrambler
     {
         static void Main(string[] args)
         {
-            //-- LISTS
-            List<int> myList = new List<int>();
-            //  adding items to list
-            myList.Add(6);
-            //  removing item from list
-            myList.Remove(1); // removes first item where value is 1 
+            string[] lines = { "This is the first line", "This is the second line", "This is the third line" };
+            File.WriteAllLines("MyFile.txt", lines);
 
-            //-- ARRAYS
-            int[] myArray = new int[3]; // array of 10 items
-            //  adding items to array
-            myArray[0] = 1;
-            myArray[1] = 4;
-            myArray[2] = 3;
+            string[] fileContent = File.ReadAllLines("MyFile.txt");
 
-            //  adding items to array, another way
-            int[] myArray2 = { 1, 4, 3 };
-
-            //  sorting array
-            Array.Sort(myArray);
-
-            //-- FOR LOOP
-            for (int i = 0; i < myArray.Count(); i++)
+            foreach (string line in File.ReadLines("MyFile.txt"))
             {
-                Console.WriteLine("Step: {0}, value: {1}", i, myArray[i]);
+                Console.WriteLine(line);
             }
-
-            //-- WHILE LOOP (if the condition is true, the loop will not be executed even once)
-            int k = 0;
-            while (k < 5) 
-            {
-                Console.WriteLine("INT: {0}", k);
-                k++;
-            }
-
-            //-- DO WHILE LOOP (will be executed at least one, even if condifition is true)
-            int l = 0;
-            do
-            {
-                Console.WriteLine("INT_2: {0}", l);
-                l++;
-            } while (l < 5);
-
-            //-- FOREACH LOOP
-            string words = "pizza";
-            foreach (char letter in words)
-            {
-                Console.WriteLine(letter);
-            }
-
-            //-- REFERENCE
-
-            //-- VALUE TYPES
 
         }
     }
